be3617298731b9ec944df4aa67f56e549a88cb7f
Likithapotla <113435495+likithapotla@users.noreply.github.com>
2022-09-14 15:37:11 +0530

    Update li.js

--- li.js
+++ li.js
@@ -1,7 +1,7 @@
 const app=require("express")();
 const PORT=process.env.PORT || 3000;
 app.get("",(req,res)=>{
-    res.send("this is Likitha from cse);
+    res.send("this is Likitha from cse");
 });
 app.listen(PORT, ()=>{
     console.log('app up at port ${PORT}');
